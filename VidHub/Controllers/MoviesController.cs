4e549eeb88b69de9506efa68a656e978c185ebd6
gnoparus <surapong@gmail.com>
2025-04-08 16:13:00 +0700

    Add Movie Controller and View.

---
+++ VidHub/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidHub.Models;
+
+namespace VidHub.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Title = "The Keng 2022!" };
+
+            return View(movie);
+        }
+    }
+}
