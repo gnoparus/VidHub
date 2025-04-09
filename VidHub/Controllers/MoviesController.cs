002223d47e0ee1ff38c09db06ab304af46c6c5fa
gnoparus <surapong@gmail.com>
2025-04-09 14:57:35 +0700

    custom route

--- VidHub/Controllers/MoviesController.cs
+++ VidHub/Controllers/MoviesController.cs
@@ -35,5 +35,10 @@ namespace VidHub.Controllers
 
             return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content("Year / Month: " + year + "/" + month);
+        }
     }
 }
